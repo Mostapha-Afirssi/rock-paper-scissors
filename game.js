526f935d4ded20484c464de5411836219ec68c89
mostapha afirssi <mustaphaafirssi@gmail.com>
2022-06-25 21:00:47 +0000

    last version of the game no icons

--- game.js
+++ game.js
@@ -1,6 +1,3 @@
-const { get } = require('prompt');
-
-const prompt = require('prompt-sync')();
 class ListNode {
     constructor(data) {
         this.data = data
@@ -22,29 +19,70 @@ function checkWinner(computerChoice,userChoice){
     else if(computerChoice.data === userChoice.data)return 0;
     return false;
 }
-function getComputerChoice(choiceList){
+function getComputerChoice(choice){
     const rotationCount = Math.floor((Math.random() * 3))
     for(j=0;j<rotationCount;j++){
-        choiceList = choiceList.next;
+        choice = choice.next;
     }
-    return choiceList;
+    return choice;
 }
 
-let scoreCount = 0
-for(i=0; i<5; i++){ 
-    let userChoice = prompt('What is your choice ?');
-    if(checkWinner(getComputerChoice(RPS),findNode(userChoice,RPS))){
-        console.log("you won");
-        scoreCount++;
-    }
-    else if(checkWinner(getComputerChoice(RPS),findNode(userChoice,RPS)) === 0){
-        console.log("draw");
-    }
-    else{
-        console.log("you lost");
-        scoreCount--;
-    }
-}
-if(scoreCount>0)console.log("you won the game");
-else if(scoreCount<0) console.log("you lost the game");
-else console.log("it's a draw");
+let userScore = 0;
+let computerScore = 0;
+let turnscount = 0;
+let userChoice;
+let computerChoice;
+const choices = document.querySelectorAll("button");
+choices.forEach((choice) => {
+    choice.addEventListener("click", function (e) { 
+        computerChoice = getComputerChoice(RPS);
+        document.getElementById(computerChoice.data).style.background = "red";
+        e.target.style.background = "green"; 
+        setTimeout(() => {
+            
+            document.getElementById(computerChoice.data).style.background = "#ECECEC";
+            e.target.style.background = "#ECECEC";
+          }, "500")  
+        turnscount++;
+        
+        if(checkWinner(computerChoice,findNode(e.target.id,RPS))){
+            document.getElementById("game_result").innerHTML = "you won" ;
+            userScore++;
+        }
+        else if(checkWinner(computerChoice,findNode(e.target.id,RPS)) === 0){
+            document.getElementById("game_result").innerHTML = "draw" ;
+        }
+        else{
+            document.getElementById("game_result").innerHTML = "lost" ;
+            computerScore++;
+        }
+        
+        document.getElementById("game_score").innerHTML = userScore + " - " + computerScore ;
+        if(turnscount >= 5 || userScore === 3 || computerScore === 3){
+            choices.forEach((choice) => {
+                choice.disabled=true;
+            })
+            if(userScore>computerScore)document.getElementById("game_result").innerHTML = 'congrat you won the game <br> <button id="restart">restart</button>' ;
+            else if(computerScore>userScore)document.getElementById("game_result").innerHTML = 'unfortunately you lost  <br> <button id="restart">restart</button>' ;
+            else document.getElementById("game_result").innerHTML = 'it\'s a draw want to start a new game ?  <br> <button id="restart">restart</button>' ;
+            const restart = document.querySelector("#restart");
+            restart.addEventListener("click", function()  {
+                turnscount = 0;
+                userScore = 0;
+                computerScore = 0;
+                choices.forEach((choice) => {
+                    choice.disabled=false;
+                })
+                document.getElementById("game_score").innerHTML = userScore + " - " + computerScore ;
+            })
+        }
+    });
+    
+});
+
+
+
+
+
+
+
